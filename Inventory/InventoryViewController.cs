c6637683ef701e23aa50af8b7b03609f67d1f6d5
Austin Makohon <austin.a.makohon@gmail.com>
2023-09-09 21:23:46 -0400

    Intentory UI changes - functionality and cosmetic

--- Inventory/InventoryViewController.cs
+++ Inventory/InventoryViewController.cs
@@ -16,11 +16,12 @@ public class InventoryViewController : MonoBehaviour
     [SerializeField] private GameObject firstContextMenuOption;
     [SerializeField] private TMP_Text itemNameText;
     [SerializeField] private TMP_Text itemDescriptionText;
-
     [SerializeField] private List<ItemSlot> itemSlots;
     [SerializeField] private ItemSlot currentSlot;
     [SerializeField] private ScreenFader fader;
     [SerializeField] private List<Button> contextMenuIgnore;
+    private Button itemSlotButton;
+    private Color itemSlotButtonDisabledColor;
 
     private enum State {
         menuClosed,
@@ -30,6 +31,20 @@ public class InventoryViewController : MonoBehaviour
 
     private State state;
 
+    public void OpenMenu() {
+        EventBus.Instance.PauseGameplay();
+        fader.FadeToBlack(0.3f, FadeToMenuCallback);
+        EventSystem.current.SetSelectedGameObject(itemSlots[0].gameObject);
+        OnSlotSelected(itemSlots[0]);
+        state = State.menuOpen;
+    }
+    public void CloseMenu() {
+        fader.FadeToBlack(0.3f, FadeFromMenuCallback);
+        state = State.menuClosed;
+        currentSlot = null;
+        itemNameText.SetText("");
+        itemDescriptionText.SetText("");
+    }
     public void UseItem() {
         fader.FadeToBlack(1f, FadeToUseItemCallback);
         
@@ -38,6 +53,7 @@ public class InventoryViewController : MonoBehaviour
         contextMenuObject.SetActive(false);
         inventoryViewObject.SetActive(false);
         FadeFromMenuCallback();
+        ButtonColorToUnselected(itemSlotButtonDisabledColor);
         EventBus.Instance.UseItem(currentSlot.itemData);
         EventSystem.current.SetSelectedGameObject(currentSlot.gameObject);
         if (currentSlot.itemData.Type == ItemData.ItemType.Consumable) {
@@ -53,6 +69,8 @@ public class InventoryViewController : MonoBehaviour
     }
     public void OnSlotSelected(ItemSlot selectedSlot) {
         currentSlot = selectedSlot;
+        itemSlotButton = currentSlot.gameObject.GetComponent<Button>();
+        itemSlotButtonDisabledColor = itemSlotButton.colors.disabledColor;
         if (selectedSlot.itemData == null) {
             itemNameText.ClearMesh();
             itemDescriptionText.ClearMesh();
@@ -61,14 +79,13 @@ public class InventoryViewController : MonoBehaviour
         itemNameText.SetText(selectedSlot.itemData.Name);
         itemDescriptionText.SetText(selectedSlot.itemData.Description[0]);
     }
+
     private void OnEnable() {
         EventBus.Instance.onPickUpItem += OnItemPickedUp;
     }
-
     private void OnDisable() {
         EventBus.Instance.onPickUpItem -= OnItemPickedUp;
     }
-
     private void OnItemPickedUp(ItemData itemData)
     {
         foreach (var slot in itemSlots) {
@@ -78,48 +95,69 @@ public class InventoryViewController : MonoBehaviour
             }
         }
     }
-
     private void Update() {
         if (Input.GetKeyDown(KeyCode.Tab)) {
             if (state == State.menuClosed) {
-                EventBus.Instance.PauseGameplay();
-                fader.FadeToBlack(0.3f, FadeToMenuCallback);
-                EventSystem.current.SetSelectedGameObject(itemSlots[0].gameObject);
-                OnSlotSelected(itemSlots[0]);
-                state = State.menuOpen;
+                OpenMenu();
             }
             else if (state == State.menuOpen) {
-                fader.FadeToBlack(0.3f, FadeFromMenuCallback);
-                state = State.menuClosed;
-                currentSlot = null;
-                itemNameText.SetText("");
-                itemDescriptionText.SetText("");
+                CloseMenu();
             }
             else if (state == State.contextMenu) {
                 contextMenuObject.SetActive(false);
                 foreach (var button in contextMenuIgnore) {
                         button.interactable = true;
                 }
+                ButtonColorToUnselected(itemSlotButtonDisabledColor);
                 EventSystem.current.SetSelectedGameObject(currentSlot.gameObject);
                 state = State.menuOpen;
             }
         }
 
+        if (Input.GetButtonDown("Cancel")) {
+            if (state == State.menuOpen) {
+                CloseMenu();
+
+            }
+            else if (state == State.contextMenu) {
+                contextMenuObject.SetActive(false);
+                    foreach (var button in contextMenuIgnore) {
+                            button.interactable = true;
+                    }
+                    EventSystem.current.SetSelectedGameObject(currentSlot.gameObject);
+                    state = State.menuOpen;
+            }
+        }
+
         // Open Context Menu
-        if (Input.GetKeyDown(KeyCode.E)) {
+        if (Input.GetButtonDown("Submit")) {
             if (state == State.menuOpen) {
                 if (EventSystem.current.currentSelectedGameObject.TryGetComponent<ItemSlot> (out var slot)) {
-                    state = State.contextMenu;
-                    contextMenuObject.SetActive(true);
-                    EventSystem.current.SetSelectedGameObject(firstContextMenuOption);
-                    foreach (var button in contextMenuIgnore) {
-                        button.interactable = false;
+                    if (currentSlot.itemData != null) {
+                        state = State.contextMenu;
+                        contextMenuObject.SetActive(true);
+                        ButtonColorToSelected(Color.red);
+                        EventSystem.current.SetSelectedGameObject(firstContextMenuOption);
+                        foreach (var button in contextMenuIgnore) {
+                            button.interactable = false;
+                        }
                     }
                 }
             }
         }
     }
-
+    private void ButtonColorToSelected(Color color)
+    {
+        var itemSlotButtonColor = itemSlotButton.colors;
+        itemSlotButtonColor.disabledColor = color;
+        itemSlotButton.colors = itemSlotButtonColor;
+    }
+    private void ButtonColorToUnselected(Color color)
+    {
+        var itemSlotButtonColor = itemSlotButton.colors;
+        itemSlotButtonColor.disabledColor = color;
+        itemSlotButton.colors = itemSlotButtonColor;
+    }
     private void FadeToMenuCallback() {
         inventoryViewObject.SetActive(true);
         fader.FadeFromBlack(0.3f, null);
